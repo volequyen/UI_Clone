40dda416b3460d547b3cf6ab0f02a51187a14444
volequyen <qvo9110@gmail.com>
2025-04-09 21:39:40 +0700

    Header UI

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,12 +1,28 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
+import { FaTiktok } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+import { IoIosCloseCircle } from "react-icons/io";
 
 const cx = classNames.bind(styles)
 
 function Header() {
     return <header className = {cx('wrapper')}>
         <div className = {cx('inner')}>
-            
+            <div className={cx('logo')}>
+                <FaTiktok />
+                <span className={cx('name')}>Tiktok</span>
+            </div>
+            <div className={cx('search')}>
+                <input placeholder='Search account and videos' spellCheck={false}/>
+                <button className={cx('clear')}>
+                    <IoIosCloseCircle />
+                </button>
+                <button className={cx('searchButton')}>
+                    <IoSearch />
+                </button>
+                
+            </div>
         </div>
     </header>
 }
